1aae1ba92b117fc83e75e53a0a4f039c41af92eb
fawwazmayda <maydafawwaz@gmail.com>
2020-03-13 17:56:20 +0700

    Making Rating Control

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,20 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Muhammad Fawwaz Mayda on 13/03/20.
+//  Copyright © 2020 Muhammad Fawwaz Mayda. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    override init(frame: CGRect) {
+        super.init(frame:frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder:coder)
+    }
+
+}
