ea757d8c79419edc260da993c76caa763de3e03f
fawwazmayda <maydafawwaz@gmail.com>
2020-03-13 21:43:25 +0700

    button star

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -12,7 +12,11 @@ import UIKit
     
     //MARK: Properties
     private var ratingButtons = [UIButton]()
-    var rating = 0
+    var rating = 0 {
+        didSet {
+            updateButtonSelectionStates()
+        }
+    }
     @IBInspectable var starSize : CGSize = CGSize(width: 44.0, height: 44.0) {
         didSet {
             setupButtons()
@@ -34,7 +38,15 @@ import UIKit
     }
     //MARK : Private Methods
     @objc func ratingButtonTapped(button: UIButton) {
-        print("Button pressed 👍")
+        guard  let index = ratingButtons.firstIndex(of: button) else {
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        let selectedRating = index + 1
+        if selectedRating == rating {
+            rating = 0
+        } else {
+            rating = selectedRating
+        }
     }
     private func setupButtons() {
         for button in ratingButtons {
@@ -42,9 +54,18 @@ import UIKit
             button.removeFromSuperview()
         }
         ratingButtons.removeAll()
+        
+        
+        let bundle = Bundle(for :type(of:self))
+        let filledStar = UIImage(named: "filledStar",in: bundle, compatibleWith:  self.traitCollection)
+        let emptyStar = UIImage(named: "emptyStar",in: bundle, compatibleWith:  self.traitCollection)
+        let highlightedStar = UIImage(named: "highlightedStar",in: bundle, compatibleWith:  self.traitCollection)
         for _ in 0..<starCount {
             let button = UIButton()
-            button.backgroundColor = UIColor.green
+            button.setImage(emptyStar, for: .normal)
+            button.setImage(filledStar, for: .selected)
+            button.setImage(highlightedStar, for: .highlighted)
+            button.setImage(highlightedStar, for: [.highlighted,.selected])
             // Add constraints
             button.translatesAutoresizingMaskIntoConstraints = false
             button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
@@ -56,6 +77,13 @@ import UIKit
             
             addArrangedSubview(button)
             ratingButtons.append(button)
+        }
+        updateButtonSelectionStates()
+    }
+    
+    private func updateButtonSelectionStates() {
+        for (index,button) in ratingButtons.enumerated() {
+            button.isSelected = index < rating
         }
     }
 }
