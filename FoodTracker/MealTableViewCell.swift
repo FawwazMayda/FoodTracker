1433884821f8aa4c84787163afa299145be7b1ae
fawwazmayda <maydafawwaz@gmail.com>
2020-03-13 23:25:08 +0700

    make a TableViewController

--- FoodTracker/MealTableViewCell.swift
+++ FoodTracker/MealTableViewCell.swift
@@ -11,9 +11,7 @@ import UIKit
 class MealTableViewCell: UITableViewCell {
     
     @IBOutlet weak var nameLabel: UILabel!
-    
     @IBOutlet weak var photoImageView: UIImageView!
-    
     @IBOutlet weak var ratingControl: RatingControl!
     
     override func awakeFromNib() {
