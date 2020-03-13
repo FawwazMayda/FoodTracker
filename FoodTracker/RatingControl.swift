655ce29686190f13d8f4846fe58ca42babc7876c
fawwazmayda <maydafawwaz@gmail.com>
2020-03-13 18:30:19 +0700

    Added 5 green buttons

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class RatingControl: UIStackView {
+    
+    //MARK: Properties
+    private var ratingButtons = [UIButton]()
+     
+    var rating = 0
     override init(frame: CGRect) {
         super.init(frame:frame)
+        setupButtons()
     }
     
     required init(coder: NSCoder) {
         super.init(coder:coder)
+        setupButtons()
     }
-
+    //MARK : Private Methods
+    @objc func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+    }
+    private func setupButtons() {
+        for _ in 0..<5 {
+            let button = UIButton()
+            button.backgroundColor = UIColor.green
+            // Add constraints
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            
+            // Add the button to the stack
+            // Setup the button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            addArrangedSubview(button)
+            ratingButtons.append(button)
+        }
+    }
 }
